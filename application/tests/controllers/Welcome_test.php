6757220aa25fc90ad3fa9e7bf0ac4f1efaa127be
umi.sz <umishoima@gmail.com>
2017-10-22 19:40:43 +0700

    test-welcome

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -5,6 +5,7 @@ class Welcome_test extends TestCase
     
         public function test_index() {
             $output = $this->request('GET', 'welcome');
+            
             $this->assertContains('<title>SIP - Login</title>', $output);
         }
         
